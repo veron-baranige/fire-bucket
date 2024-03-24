0f997f4f45abe8d0f0bfaf6442624f67fcc20f1c
Veron Baranige <veron.b@hsenidoutsourcing.com>
2024-03-24 19:06:56 +0530

    Add server with initial swagger doc

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -4,10 +4,22 @@ import (
 	"log/slog"
 	"os"
 
+	"github.com/labstack/echo/v4"
+	"github.com/labstack/echo/v4/middleware"
 	"github.com/lmittmann/tint"
+	echoSwagger "github.com/swaggo/echo-swagger"
+
+	_ "github.com/veron-baranige/fire-bucket/docs/swagger"
 	"github.com/veron-baranige/fire-bucket/internal/config"
 )
 
+// @title Fire-Bucket
+// @version 1.0
+// @description API for uploading and retrieving files through Firebase Storage
+// @contact.name Veron Baranige
+// @contact.email veronsajendra@gmail.com
+// @host http://localhost:8000
+// @BasePath /api
 func main() {
 	setupLogger()
 
@@ -15,9 +27,21 @@ func main() {
 		slog.Error("Failed to load environment variables", "err", err)
 	}
 	slog.Info("Loaded configurations")
+
+	e := echo.New()
+	e.Use(middleware.Recover())
+	setupRoutes(e)
+
+	if err := e.Start(":" + config.Get(config.ServerPort)); err != nil {
+		slog.Error("Failed to start the server", "err", err)
+	}
 }
 
 func setupLogger() {
 	logger := slog.New(tint.NewHandler(os.Stderr, nil))
 	slog.SetDefault(logger)
+}
+
+func setupRoutes(e *echo.Echo) {
+	e.GET("/swagger/*", echoSwagger.WrapHandler)
 }
